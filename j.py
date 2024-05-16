ba63b6c74f14ab9d47f2d269fddd04d55d33979b
chaithunaiduu <19p0645chaiyasara@gmail.com>
2024-05-16 10:44:27 +0530

    Create j.py

---
+++ j.py
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Background Image Example</title>
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    background-image: url('RCB.jpg');
+    background-repeat: no-repeat;
+        
+    background-position: center;
+    font-family: Arial, sans-serif;
+    color: Black; /* text color */
+  }
+  .content {
+    text-align: center;
+    padding: 50px;
+  }
+  h1 {
+    font-size: 36px;
+  }
+  p {
+    font-size: 18px;
+  }
+</style>
+</head>
+
+<body>
+         <h1>Welcome to RCB  TEAM </h1>
+    <p>VIRAT KOLIE </p>
+    <p>CAPATAIN OF THE TEAM</p>
+      
+    <p><a href="y.html">Go back to Page 1</a></p>
+</body>
+</html>
